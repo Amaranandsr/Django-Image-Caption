6fb8f8599d5102b6dfa5ba7fba52f11d5d03b881
Amar Anand <47771942+Amaranandsr@users.noreply.github.com>
2021-12-07 14:04:23 +0530

    setting

--- CaptionBot/settings.py
+++ CaptionBot/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9m35u%%4xhc#*l044s%!vrm@fsjpttdoxkn=jk&z8uewp0*1!@'
+SECRET_KEY = ' '
+# 9m35u%%4xhc#*l044s%!vrm@fsjpttdoxkn=jk&z8uewp0*1!@
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
